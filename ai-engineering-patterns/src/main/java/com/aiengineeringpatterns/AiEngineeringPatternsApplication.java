b88756acdaae4d7395b14e8bff18aeb3e3bf2683
srinivas-tatraju <tsr.5n.dev@gmail.com>
2026-07-27 18:36:54 -0400

    Initialize Spring Boot project

---
+++ ai-engineering-patterns/src/main/java/com/aiengineeringpatterns/AiEngineeringPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.aiengineeringpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiEngineeringPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiEngineeringPatternsApplication.class, args);
+	}
+
+}
---
+++ ai-engineering-patterns/src/test/java/com/aiengineeringpatterns/AiEngineeringPatternsApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.aiengineeringpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiEngineeringPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
